fd8e1efc4f712acf9c662e093de2bcf311266cdc
dstearle <dallas.st.earle@gmail.com>
2020-05-01 19:15:24 -0700

    Began markup for main app file

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
 
-  <div id="app">
-  </div>
+	<div id="app">
+
+		<main>
+
+			<div class="search-box">
+
+				<input type="text" class="search-box" placeholder="Search..."/>
+
+			</div>
+
+		</main>
+
+	</div>
 
 </template>
 
 <script>
 
-  export default {
-    name: 'App',
-  }
+	export default {
+
+		name: 'App',
+
+		data() {
+
+			return {
+
+				api_key: '872e27b99ee063355a94208b358ae359'
+
+			}
+
+		}
+
+	}
 
 </script>
 
 <style>
-
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-
 </style>
